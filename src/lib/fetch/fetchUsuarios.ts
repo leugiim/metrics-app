9c4f60ced21064d1de4004e314fea4b51272e97b
Miguel Rodriguez <miguel.rodriguezl@masmovil.com>
2023-03-16 13:45:48 +0100

    edit function to arrow function syntax

--- src/lib/fetch/fetchUsuarios.ts
+++ src/lib/fetch/fetchUsuarios.ts
@@ -1,6 +1,6 @@
 import { useFetch } from '$lib/fetch';
 import { type Usuario, usuarioSchema } from '$lib/types/Usuario';
 
-export function useFetchUsuario() {
+export const useFetchUsuario = () => {
 	return useFetch<null, Usuario>(usuarioSchema, 'https://pokeapi.co/api/v2/pokemon/ditto');
-}
+};
